b65e668a9d86d3252a7ddaeac4e89bc3d8d5117e
Adrien Charruel <adrien.charruel@gmail.com>
2020-05-08 16:49:23 +0200

    Add opensubtitles option

--- sub-downloader.sh
+++ sub-downloader.sh
@@ -3,6 +3,7 @@ set -e
 echo `dirname $0`
 declare LOG_FILE=`dirname $0`/sub-downloader.log
 declare WANTED_FILE=`dirname $0`/wanted/subs.wanted
+declare OPTIONS='--opensubtitles USERNAME PASSWORD'
 
 # Sonarr does not show the stdout as part of the log information displayed by the system,
 # So I decided to store the log information by my own.
@@ -31,9 +32,8 @@ fi
 doLog "Looking for subtitles for: ${EPISODE_PATH}"
 
 doLog "Executing subliminal"
-doLog "subliminal download ${LANGUAGES} ${EPISODE_PATH}"
 for lang in $LANGUAGES; do
-    subliminal download -l ${lang} "${EPISODE_PATH}" >> $LOG_FILE 2>&1
+    subliminal ${OPTIONS} download -l ${lang} "${EPISODE_PATH}" >> $LOG_FILE 2>&1
 done
 
 # Look for not found subtitles
@@ -46,3 +46,4 @@ for LANG in "${LANG_ARRAY[@]}"; do
         echo $EPISODE_PATH:$SUB_FILE >> ${WANTED_FILE}
     fi
 done
+
