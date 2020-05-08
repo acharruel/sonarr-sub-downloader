54990028f70ae15c3693ac7508c68f23fef6e51a
Adrien Charruel <adrien.charruel@gmail.com>
2020-05-08 16:40:00 +0200

    Adapt script to my own sonarr use...

--- sub-downloader.sh
+++ sub-downloader.sh
@@ -7,59 +7,42 @@ declare WANTED_FILE=`dirname $0`/wanted/subs.wanted
 # Sonarr does not show the stdout as part of the log information displayed by the system,
 # So I decided to store the log information by my own.
 function doLog {
-  echo -e $1
-  echo -e $1 >> $LOG_FILE
+    echo -e $1
+    echo -e $1 >> $LOG_FILE
 }
 
-function printUsage {
-  msg="Usage: sub-downloader.sh [options]\n\n
-    -l, --languages <languages-list>:\n
-    \t Specify a comma-separated list of languages to download.\n
-    \t example: sub-downloader.sh -l es,en\n\n
-    -h, --help: print this help"
-  doLog "$msg"
-  exit 1
-}
-
-if [[ $# -eq 0 ]]; then
-  printUsage
-fi
-
-while [ "$1" != "" ]; do
-  case $1 in
-    "-l" | "--languages")
-      shift
-      declare LANGUAGES=$(echo "-l $1" | sed "s/,/ -l /g")
-      ;;
-    *)
-      printUsage
-      ;;
-  esac
-  shift
-done
+declare LANGUAGES="en fr"
 
 doLog "###### Process started at: $(date) ######"
 
 declare EPISODE_PATH=${sonarr_episodefile_path}
+declare EVENT=${sonarr_eventtype}
+
+if [ $EVENT == "Test" ]; then
+    doLog "Test from sonarr"
+    exit 0
+fi
 
 if [[ -z $EPISODE_PATH ]]; then
-  doLog "sonarr_episodefile_path environment variable not found"
-  exit 1
+    doLog "sonarr_episodefile_path environment variable not found"
+    exit 1
 fi
 
 doLog "Looking for subtitles for: ${EPISODE_PATH}"
 
 doLog "Executing subliminal"
 doLog "subliminal download ${LANGUAGES} ${EPISODE_PATH}"
-subliminal download ${LANGUAGES} "${EPISODE_PATH}" >> $LOG_FILE 2>&1
-  
+for lang in $LANGUAGES; do
+    subliminal download -l ${lang} "${EPISODE_PATH}" >> $LOG_FILE 2>&1
+done
+
 # Look for not found subtitles
 declare LANG_ARRAY=($(echo ${LANGUAGES} | sed "s/-l //g"))
 
 for LANG in "${LANG_ARRAY[@]}"; do
-  SUB_FILE=$(echo $EPISODE_PATH | sed "s/...$/${LANG}\.srt/g")
-  if [[ ! -f $SUB_FILE ]]; then
-    doLog "Subtitle ${SUB_FILE} not found, adding it to wanted"
-    echo $EPISODE_PATH:$SUB_FILE >> ${WANTED_FILE}
-  fi
+    SUB_FILE=$(echo $EPISODE_PATH | sed "s/...$/${LANG}\.srt/g")
+    if [[ ! -f $SUB_FILE ]]; then
+        doLog "Subtitle ${SUB_FILE} not found, adding it to wanted"
+        echo $EPISODE_PATH:$SUB_FILE >> ${WANTED_FILE}
+    fi
 done
